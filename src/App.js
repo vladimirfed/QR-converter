660f472561f7d1945c2d49731c9ba80f08473403
Vladimir Fedarenka <vfedarenka074@gmail.com>
2022-03-26 18:01:13 +0100

    initialt

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import QRCode from "react-qr-code";
 
 function App() {
+
+  const [inputValue, setInputValue] = React.useState('')
+
   return (
-    <div className="container mx-auto w-2/4">
-        <h1 className="py-4">QR Converter </h1>
-        <input type="text"
-              placeholder="Fill the input for QR code"
-              className="border border-gray-300 w-full h-8 p-4 font-light text-sm focus: outline-none focus: border-gray-600" ></input>
-      
+    <div className="App">
+      <h1 className="py-4">QR Converter </h1>
+      <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></link>
+      <div className="form__group">
+        <input
+          onChange={e=>setInputValue(e.target.value)}
+          type="text"
+          className="form__input"
+          id="name"
+          placeholder="Enter  the link"
+          required=""
+        />
+        <QRCode 
+          className="App"
+          value={inputValue} />
+      </div>
     </div>
   );
 }
